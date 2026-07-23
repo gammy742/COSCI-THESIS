f7403fedac9af1f0c37b1ab4296875dbce5ec08c
gammy742 <Voranate.thaiprasert2546@gmail.com>
2026-07-23 16:52:18 +0700

    add sys in main

--- api/main.py
+++ api/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from flask import Flask,jsonify,request
 from dotenv import load_dotenv
 import os
@@ -11,7 +15,6 @@ CORS(app)
 
 
 from adminlogin import admin_api
-#ADMIN_login api
 app.register_blueprint(admin_api, url_prefix="/api/admin")
 
 from countdown import countdown_api
@@ -20,46 +23,36 @@ app.register_blueprint(countdown_api, url_prefix="/api")
 from booth import booth_api
 app.register_blueprint(booth_api, url_prefix="/api")
 
-#dbconect
 from getdb import get_db
 
-#join api
 from join import join_api
 app.register_blueprint(join_api, url_prefix="/api")
 
-#scan api
 from scan import scan_api
 app.register_blueprint(scan_api, url_prefix="/api")
 
-#scan api
 from scan import progress_api
 app.register_blueprint(progress_api, url_prefix="/api")
 
-#excel-export api
 from excel import excel_api
 app.register_blueprint(excel_api, url_prefix="/api")
 
-#allmember api
 from member import all_members_api
 app.register_blueprint(all_members_api, url_prefix="/api")
 
-#boothmember api
 from member import booth_members_api
 app.register_blueprint(booth_members_api, url_prefix="/api")
 
-#reward
 from rewardy import reward_api
 app.register_blueprint(reward_api, url_prefix="/api")
 
-#Get_IP
+
 def get_client_ip():
     forwarded = request.headers.get("X-Forwarded-For")
     if forwarded:
-        # X-Forwarded-For อาจส่งมาหลาย IP ให้เอาตัวแรกสุด
         return forwarded.split(",")[0].strip()
     return request.remote_addr
 
-#vercel connect
 @app.route("/")
 def home():
     return {
